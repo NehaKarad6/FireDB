3d25d95c914e64f3e5d828a28e0828f425b0d6d6
NehaKarad6 <karad.neha6@gmail.com>
2017-04-08 23:01:12 +0530

    Initial commit

---
+++ app/src/main/java/com/example/neha/firedb/MainActivity.java
@@ -0,0 +1,55 @@
+package com.example.neha.firedb;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.database.DatabaseError;
+
+public class MainActivity extends AppCompatActivity {
+    private DatabaseReference mDatabase;
+    private String firstname;
+    private String email;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+
+        findViewById(R.id.btnsubmit).setOnClickListener(this::Click);
+        findViewById(R.id.btnsubmit).setOnClickListener(this::Display);
+    }
+
+    private void Click(View view) {
+        firstname=((EditText) findViewById(R.id.edtname)).getText().toString();
+        email=((EditText) findViewById(R.id.edtemail)).getText().toString();
+        User user = new User(firstname, email);
+
+        mDatabase.child("users").child(firstname).setValue(user);
+
+    }
+
+    public void Display(View view) {
+        mDatabase.child("mobiles").child("12").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                User user = dataSnapshot.getValue(User.class);
+                Log.i("@codekul", ""+user);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+
+}
